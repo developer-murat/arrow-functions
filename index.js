57b54c7e14edde873735f0fdc0d66a1628f21dad
murat demir <murat_demir1985@hotmail.com>
2024-01-13 19:44:58 +0300

    update

---
+++ index.js
@@ -0,0 +1,20 @@
+//  Arrow Functions (oklu fonskiyon ifadeleri)
+
+// console.log(topla(3, 5));
+
+// function topla(sayi1, sayi2) {
+//     return sayi1 + sayi2;
+// }
+
+// const topla = (sayi1, sayi2) => {
+//     return sayi1 + sayi2;
+// };
+
+// console.log(topla(3, 5));
+
+const topla = (sayi1, sayi2) => sayi1 + sayi2;
+
+console.log(topla(3, 5));
+
+const kare = a => a * a;
+console.log(kare(2));
